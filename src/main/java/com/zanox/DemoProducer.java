6450879bf6cd0821a4c4d1b671e7f0384deb332a
Ruslan Dereviakin <Ruslan.Dereviakin@zanox.com>
2015-07-09 09:59:42 +0200

    Fixed: correct java version, Added: Groovy console to play with "producer.sendNotification(from, to, body)"

--- src/main/java/com/zanox/DemoProducer.java
+++ src/main/java/com/zanox/DemoProducer.java
@@ -1,5 +1,6 @@
 package com.zanox;
 
+import groovy.ui.Console;
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
@@ -8,13 +9,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
+import groovy.lang.Binding;
 
 public class DemoProducer {
 
+    private Producer<String, byte[]> producer = new Producer<String, byte[]>(configure());
 
     public static void main(String args[]) {
         DemoProducer producer = new DemoProducer();
+
+        // Generates a few notifications
         producer.run();
+
+        // Console window to play with "produces"
+        Binding binding = new Binding();
+        binding.setVariable("producer", producer);
+        Console console = new Console(binding);
+        console.run();
     }
 
     private ProducerConfig configure() {
@@ -27,28 +38,32 @@ public class DemoProducer {
     }
 
     private void run() {
-        ProducerConfig config = configure();
-
-        Producer<String, byte[]> producer = new Producer<String, byte[]>(config);
-        for(User.Notification notification : generateNotifications(5)) {
-            System.out.println("notification: " + notification);
+        for(User.Notification notification : generateNotificationList(5)) {
+            System.out.println("Generated: " + notification);
             producer.send(new KeyedMessage<String, byte[]>("trainingKafka3ada", notification.toByteArray()));
         }
 
     }
 
-    private static Iterable<User.Notification> generateNotifications(int n) {
+    public void sendNotification(String from, String to, String body) {
+      producer.send(new KeyedMessage<String, byte[]>("trainingKafka3ada", generateNotification(from, to, body).toByteArray()));
+    }
+
+    private static Iterable<User.Notification> generateNotificationList(int n) {
         List<User.Notification> notifications = new ArrayList<User.Notification>();
         for(int i = 0; i < n; i++) {
-            notifications.add(
-                    User.Notification.newBuilder()
-                            .setMessageId(UUID.randomUUID().toString())
-                            .setFromUser("System")
-                            .setToUser("whoeever@service.com")
-                            .setBody("Hello " + i)
-                            .setSendOn(System.currentTimeMillis())
-                            .build());
+            notifications.add(generateNotification("System", "whoeever@service.com", "Hello" + i));
         }
         return notifications;
+    }
+
+    private static User.Notification generateNotification(String from, String to, String body) {
+      return User.Notification.newBuilder()
+              .setMessageId(UUID.randomUUID().toString())
+              .setFromUser(from)
+              .setToUser(to)
+              .setBody(body)
+              .setSendOn(System.currentTimeMillis())
+              .build();
     }
 }
